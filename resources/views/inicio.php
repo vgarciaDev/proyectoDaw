0bd647c7d354aced33fc74d45c9a490d636bf0a5
vgarciaDev <victor.garciag93@gmail.com>
2024-03-01 18:58:22 +0100

    Plantilla

--- resources/views/inicio.php
+++ resources/views/inicio.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-</head>
-<body>
-    <h1>Apartado Inicio</h1>
-</body>
-</html>
+@extends('template')
+
+@section('title') Inicio @endsection
+
+@section('content')
+    <div class="container mt-4">
+        <h1>Página de inicio</h1>
+        <p>Bienvenido a mi aplicación</p>
+    </div>
+@endsection
