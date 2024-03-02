948a6e9685d62191fb841e8a4e6686839117ff89
vgarciaDev <victor.garciag93@gmail.com>
2024-03-02 10:25:47 +0100

    modificaciones header

--- resources/views/talento.blade.php
+++ resources/views/talento.blade.php
@@ -4,7 +4,18 @@
 
 @section('content')
     <div class="container mt-4">
-        <h1>Página de talento</h1>
-        <p>Bienvenido a mi aplicación</p>
+        <div class="row mt-5">
+            <div class="col-6  ">
+                <div>
+                    <img class="img-head" src="https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_1280.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-6">
+                <h2 class="text-justify-h2">Creemos en la capacidad y potencial del talento para forjar un mañana más prometedor tanto 
+                    para individuos como para empresas en todo el mundo. En Royal Tech, trabajamos para impulsar oportunidades significativas 
+                    mediante el uso innovador de la tecnología.</h2>
+            </div>
+            
+        </div>
     </div>
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/talento.css') }}">
+    <style>body{margin-top: -15px;}</style>
 </head>
 <body>
     <header>
@@ -20,5 +22,11 @@
     <footer>
         @include('templates.footer')
     </footer>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script>
+        @yield('script')
+    </script>
+
 </body>
 </html>
