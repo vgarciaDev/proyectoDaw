4af3f7e124f70548b41cf6084c8da5f6559b14cc
vgarciaDev <victor.garciag93@gmail.com>
2024-03-02 11:16:13 +0100

    nuevo update talento

--- resources/views/talento.blade.php
+++ resources/views/talento.blade.php
@@ -9,13 +9,12 @@
                 <img class="img-tech" src="{{ asset('img/imagen-tech.png') }}" alt="Imagen">   
             </div>
             <div class="col-6 mt-5">
-                <h2><b>¿Quieres trabajar para Royal Tech?</b></h2>
+                <h2 class="text-center"><b>¿Quieres trabajar para Royal Tech?</b></h2>
                 <hr class="underline">
                 <h2 class="text-justify-h2 mt-5">Creemos en la capacidad y potencial del talento para forjar un mañana más prometedor tanto 
                     para individuos como para empresas en todo el mundo. En Royal Tech, trabajamos para impulsar oportunidades significativas 
                     mediante el uso innovador de la tecnología.</h2>
             </div>
-            
         </div>
     </div>
 @endsection
