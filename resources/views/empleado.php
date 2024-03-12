d9d3cadb3ea02e968263ef10582ba956c9f3f91d
Vane5922 <vanesaortizmontoya@gmail.com>
2024-03-12 20:21:08 +0100

    -

--- resources/views/empleado.php
+++
@@ -1,9 +0,0 @@
-@extends('templates.template')
-
-@section('title') Empleado @endsection
-
-@section('content')
-    
-
-    
-@endsection
---
+++ resources/views/empleados.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.template')
+
+@section('title') Nuestro Trabajo @endsection
+
+@section('content')
+    <div class="container">
+        <h1>PRUEBAAAAAAA</h1>
+    </div>
+
+
+    
+@endsection
