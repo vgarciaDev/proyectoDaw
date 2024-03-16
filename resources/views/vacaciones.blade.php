4871a37ab7880c704fe6332f7f6d5b0620659c0d
Adrian Bourdelande <adrianbourdelande@gmail.com>
2024-03-16 12:28:38 +0100

    Anadir dias funcionando

--- resources/views/vacaciones.blade.php
+++ resources/views/vacaciones.blade.php
@@ -18,77 +18,97 @@
             </div>  
             <div class="col-6">
                 {{-- <smart-calendar id="calendar" class="menu" @click="handleClick"></smart-calendar> --}}
-                <smart-calendar months="4" @click="handleClick"></smart-calendar>
+                <smart-calendar 
+                id='calendario'
+                theme='blue' 
+                months='4'
+                locale='es'
+                first-day-of-week='1'>
+                </smart-calendar>
             </div> 
             <div class="col-3" id="menuDias">
-                <button class="btn btn-bd-primary"  @click="seleccionClick">Seleccionar días</button>
+                <button class="btn btn-bd-primary" id="seleccionarDiasButton">Seleccionar días</button>
                 <div id="cajonDias">
                     <div>dias seleccionados</div>
-                    <ul>
+                    <ul id="listaDias">
                         <!-- Utiliza v-for para iterar sobre cada elemento del array y renderizarlos como elementos de lista -->
-                        
                     </ul>
                 </div>
-
-                    
             </div> 
-            
-        </div>        
-        
+        </div>   
     </div>
-    
-
 @endsection
 
 @section('script')
-
 <script src="/proyectoDAW/node_modules/smart-webcomponents/source/modules/smart.calendar.js"></script>
 <script>
     let diasSeleccionados = [];
     let seleccion = false;
     let cajonDias = document.getElementById('cajonDias');
+    const calendario = document.getElementById('calendario');
+    const seleccionarDiasButton = document.getElementById('seleccionarDiasButton');
+    const listaDias = document.getElementById('listaDias');   
     
-    
-    const app = Vue.createApp({
-        
-        // data() {
-        //     return {
-        //         form: {
-        //             nombre: "",
-        //             apellidos: "",
-        //             telefono: "",
-        //             email: "",
-        //             opciones: "",
-        //             mensaje: ""
-        //         }
-        //         }
-        //     }   
-        methods: {
-            handleClick(event) {  
-                            
-                // Verifica si el elemento clickeado o alguno de sus ancestros tiene la clase 'elemento'
-                if (event.target.closest('.smart-calendar-week')&&seleccion==true) {
-                    console.log(event);
-                    const dia = Number(event.srcElement.value.getDate());
-                    const mes = Number(event.srcElement.value.getMonth())+1;
-                    const year = Number(event.srcElement.value.getFullYear());
-                    const element = `${dia}.${mes}.${year}`;
-                    // console.log(`${dia}.${mes}`);
-                    diasSeleccionados.push(element);
-                    console.log(diasSeleccionados);
-                    this.pintarDias();
-                    
-                }
-
-            }
-            ,
-            seleccionClick(event){
-                seleccion = true;
-            }
+    //Introduce el dia sobre el que se clicka en un array
+    calendario.addEventListener("click", (event) => {
+        if (event.target.closest('.smart-calendar-week')&&seleccion==true) {
+            console.log(event);
+            const dia = Number(event.srcElement.value.getDate());
+            const mes = Number(event.srcElement.value.getMonth())+1;
+            const year = Number(event.srcElement.value.getFullYear());
+            const element = `${dia}.${mes}.${year}`;
+            diasSeleccionados.push(element);
+            console.log(diasSeleccionados);
+            pintarDias();            
         }
+    }); 
+
+    //Activa/Desactiva la posibilidad de seleccionar dias o no
+    seleccionarDiasButton.addEventListener("click", (event) => {
+        if(seleccion == true){
+            seleccion = false;
+            seleccionarDiasButton.style.color = 'blue'
+        }else{
+            seleccion = true;
+            seleccionarDiasButton.style.color = 'red'
+        }
+    })
+    
+    //Monitorea si se clicka sobre el icono de la cruz para borrar algun dia del array diasSeleccionados
+    listaDias.addEventListener('click', function(event) {
+        console.log(event);
+        if (event.target.parentElement.className === 'diaSeleccionado') {
+            diasSeleccionados =  diasSeleccionados.filter(dia => dia != event.target.parentElement.innerText );
+        }
+        pintarDias();
     });
 
-    app.mount('#calendar');
+    //Pinta los días seleccionados en un div a la derecha del calendario
+    function pintarDias(){
+        listaDias.innerHTML = '';
+        diasSeleccionados.forEach((dia, index) => {
+            const itemList = document.createElement('li');
+            itemList.innerHTML = dia;
+            itemList.id ="itemList_"+ index;
+            itemList.classList.add("diaSeleccionado");
+            itemList.style.listStyleType = 'none';
+            const img = document.createElement('img');
+            img.src = '{{ asset('/img/vacaciones/icons8-delete-16.png') }}';
+            itemList.appendChild(img);
+            listaDias.appendChild(itemList);
+        });
+    }
+        
+    
+                
+
+        
+    
+    
+    
+
+            
+
 </script>
 
 @endsection
