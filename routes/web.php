464915188f3f1e3c5b41a6c5dbc084556a3f250a
vgarciaDev <victor.garciag93@gmail.com>
2024-03-21 20:46:50 +0100

    template mezclado

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,10 @@ Route::get('/login', LoginController::class);
 Route::get('/inicio', InicioController::class);
 Route::get('/nuestroTrabajo', NuestroTrabajoController::class);
 Route::get('/talento', [TalentoController::class, 'index']);
-
-Route::post('/talento', [TalentoController::class, 'setCv']);
-Route::get('/contacto', ContactoController::class);
+Route::post('/talento', [TalentoController::class, 'setCv'])->name("talento");
 Route::get('/templateERP', templateERPController::class);
 Route::get('/acceso', accesoController::class);
 
-
-
-Route::post('/talento', [TalentoController::class, 'setCv'])->name("talento");
 Route::get('/contacto', [ContactoController::class, 'index']);
 Route::post('/contacto', [ContactoController::class, 'sendEmailFn']);
 Route::get('/fichaje', [FichajeController::class, 'index']);
