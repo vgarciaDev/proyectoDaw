c1e9cd4f18721ab395b02adf774dfe3c77d72757
Adrian Bourdelande <adrianbourdelande@gmail.com>
2024-03-21 20:22:31 +0100

    mejoras implementadas

--- app/Http/Controllers/Vacacionescontroller.php
+++ app/Http/Controllers/Vacacionescontroller.php
@@ -13,11 +13,14 @@ class Vacacionescontroller extends Controller
     public function enviarDatos(Request $request)
     {
         
-        $diasSeleccionados = $request->input('mi_array');
+        $diasSeleccionados = $request->input('arrayDiasSeleccionados');
         $vacaciones = new vacaciones();
-        $vacaciones->user = 1;
-        $vacaciones->vacaciones =$diasSeleccionados[1];
+        //En user habrá que meter luego el usuario con el que estamos logueados
+        $vacaciones->user = 'Adrian';
+        $diasSeleccionadosString = implode(" ", $diasSeleccionados);       
+        $vacaciones->vacaciones = $diasSeleccionadosString;
         $vacaciones->save();
+        
         
         // Procesa el array como desees, por ejemplo, puedes guardarlo en la base de datos
 
--- resources/views/vacaciones.blade.php
+++ resources/views/vacaciones.blade.php
@@ -1,26 +1,18 @@
 @extends('templateERP')
 
-
-
 @section('menuElegido')
     <div class="container-fluid menu" id="calendar">
         <div class="row">
-            {{-- <div class="col-3">
-                <div>MENU</div>    
-                <div>MENU</div>    
-                <div>MENU</div>    
-                <div>MENU</div>    
-                <div>MENU</div>    
-                <div>MENU</div>    
-            </div>   --}}
             <div class="col-6">
-                {{-- <smart-calendar id="calendar" class="menu" @click="handleClick"></smart-calendar> --}}
                 <smart-calendar 
                 id='calendario'
                 theme='blue' 
                 months='4'
                 locale='es'
-                first-day-of-week='1'>
+                first-day-of-week='1'
+                theme='red'
+                selection-mode='one'
+                month-name-format='numeric'>
                 </smart-calendar>
             </div> 
             <div class="col-3" id="menuDias">
@@ -28,7 +20,6 @@
                 <div id="cajonDias">
                     <div>dias seleccionados</div>
                     <ul id="listaDias">
-                        <!-- Utiliza v-for para iterar sobre cada elemento del array y renderizarlos como elementos de lista -->
                     </ul>
                 </div>
                 <button class="btn btn-bd-primary" id="enviarDiasButton">Enviar días</button>
@@ -46,19 +37,21 @@
     let cajonDias = document.getElementById('cajonDias');
     const calendario = document.getElementById('calendario');
     const seleccionarDiasButton = document.getElementById('seleccionarDiasButton');
-    const listaDias = document.getElementById('listaDias');   
+    const listaDias = document.getElementById('listaDias');  
+    
     
     //Introduce el dia sobre el que se clicka en un array
     calendario.addEventListener("click", (event) => {
         if (event.target.closest('.smart-calendar-week')&&seleccion==true) {
-            console.log(event);
             const dia = Number(event.srcElement.value.getDate());
             const mes = Number(event.srcElement.value.getMonth())+1;
+            //switch para pasar los nombres de los meses a numeros
+
             const year = Number(event.srcElement.value.getFullYear());
-            const element = `${dia}.${mes}.${year}`;
+            const element = `${year}-${mes}-${dia}`;
             diasSeleccionados.push(element);
             console.log(diasSeleccionados);
-            pintarDias();            
+            pintarDias(); 
         }
     }); 
 
@@ -71,6 +64,7 @@
             seleccion = true;
             seleccionarDiasButton.style.color = 'red'
         }
+        pintarDias();   
     })
     
     //Monitorea si se clicka sobre el icono de la cruz para borrar algun dia del array diasSeleccionados
@@ -79,7 +73,7 @@
         if (event.target.parentElement.className === 'diaSeleccionado') {
             diasSeleccionados =  diasSeleccionados.filter(dia => dia != event.target.parentElement.innerText );
         }
-        pintarDias();
+        pintarDias();        
     });
 
     //Pinta los días seleccionados en un div a la derecha del calendario
@@ -96,20 +90,37 @@
             itemList.appendChild(img);
             listaDias.appendChild(itemList);
         });
+        pintarDiasFondo(diasSeleccionados, '#00fefb');
+    }
+
+    //Poner y quitar color de fondo de los dias seleccionados
+    function pintarDiasFondo(array, color){  
+        const dias = document.querySelectorAll('.smart-calendar-cell');
+        for(i=0;i<dias.length;i++){                
+            dias[i].style.backgroundColor = "";            
+        } 
+        array.forEach(item =>{
+            for(i=0;i<dias.length;i++){
+            
+                if(dias[i].innerHTML==item.split('-')[2] && 
+                dias[i].parentElement.parentElement.parentElement.children[0].innerHTML.split(' ')[0]==item.split('-')[1]){
+                    dias[i].style.backgroundColor = color;
+                }
+                
+            
+            }
+        })
     }
     
     $(document).ready(function() {
         $('#enviarDiasButton').click(function(e) {
             e.preventDefault();
-
-            // var miArray = ['valor1', 'valor2', 'valor3']; // Tu array aquí
-
             $.ajax({
                 url: "{{ route('enviarDatos.ajax') }}",
                 type: "POST",
                 data: {
                     _token: "{{ csrf_token() }}",
-                    mi_array: diasSeleccionados
+                    arrayDiasSeleccionados: diasSeleccionados
                 },
                 success: function(response) {
                     console.log(response);
@@ -122,15 +133,5 @@
             });
         });
     });
-    
- 
-        
-    
-    
-    
-
-            
-
 </script>
-
 @endsection
