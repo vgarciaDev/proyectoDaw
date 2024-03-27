8ce1728a6f74b09ea97f9c9c2e440a95cf63558a
Vane5922 <vanesaortizmontoya@gmail.com>
2024-03-27 11:07:35 +0100

    Enlace a inicio, al logotipo de la cabecera

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid header">
   <nav class="navbar navbar-expand-lg navbar-light  ">
-      <a class="navbar-brand" href="#"><img class="w-25 ms-3" src="{{ asset('img/Logo.png') }}" alt=""></a>
+      <a class="navbar-brand" href="{{ url('/') }}"><img class="w-25 ms-3" src="{{ asset('img/Logo.png') }}" alt=""></a>
       <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon text-light"></span>
       </button>
