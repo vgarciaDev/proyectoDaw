ededaaa573733e191bbf2c269640d903740ff682
vgarciaDev <victor.garciag93@gmail.com>
2024-04-08 19:01:15 +0200

    cambios en Template 2

--- resources/views/templateERP.blade.php
+++ resources/views/templateERP.blade.php
@@ -8,10 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/768e9ae614.js" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     @yield('css')
-
 </head>
 <body>
     
@@ -19,23 +17,18 @@
     <!--inicio contenedor general-->
     <div class="row">
 
-        <div class="container text-center topbar">
-            
         <div class="container logo text-center">
-
             <img class="img-logo" src="{{ asset('img/Logo.png') }}" alt="Imagen">
-            
         </div>
 
         <div class="container-fluid">
             <!--LADO IZQUIERDO-->
             <div class="row">
-                <div class="col-3 sec-usuario">
+                <div class="col-3">
 
                     <img class="img-usuario" src="{{ asset('img/inicio/imagen_usuario.png') }}" alt="Imagen">
-                    <p class="nombres"><i class="fa-solid fa-user"></i> Gustavo Tejera Martínez</p>
+                    <p class="nombres"><i class="fa-solid fa-user"></i> Gustavo Tejera Martínes</p>
                     <!--INICIO SECCIÓN DE BOTONES-->
-
                     <!--boton FICHAR-->
                     <a href="{{ url('/fichaje') }}"><button  class="seccion pb-3"><i class="fa-solid fa-clock" id="fichaje"></i> Fichaje</button></a>
                     <!--boton VACACIONES-->
@@ -46,8 +39,10 @@
                     <a href="{{ url('/cursos') }}"><button  class="seccion pb-3"><i class="fa-solid fa-book" id="cursos"></i> Cursos</button></a>
 
                 </div>
-                <!--INICIO SECCION MOSTRAR INFORMACIÓN-->
 
+                
+
+                <!--INICIO SECCION MOSTRAR INFORMACIÓN-->
                 <div class="col-9 info">
                     @yield('content')
                 </div>
